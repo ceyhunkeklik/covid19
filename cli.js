f490ed60bacc04c3c7cce0e495b329e38efe47da
Ceyhun Keklik <ceyhun.keklik@commencis.com>
2020-04-05 01:53:23 +0300

    feat: update data source

--- cli.js
+++ cli.js
@@ -5,6 +5,7 @@ const Spinner = require("clui").Spinner;
 const Table = require("cli-table");
 const argv = require("minimist")(process.argv.slice(2));
 const chalk = require("chalk");
+const fetchData = require("./src/fetch");
 
 var countdown = new Spinner("Loading...  ", [
   "⣾",
@@ -14,57 +15,51 @@ var countdown = new Spinner("Loading...  ", [
   "⡿",
   "⣟",
   "⣯",
-  "⣷"
+  "⣷",
 ]);
 
-const filterProp = "displayName";
-const displayProp = "displayName";
+const { filterProp, displayProp } = require("./src/config");
 
 countdown.start();
 
-const getValue = country => [
+const getValue = (country) => [
   chalk.yellow.bold(country.totalConfirmed ? country.totalConfirmed : 0),
   chalk.green.bold(country.totalRecovered ? country.totalRecovered : 0),
-  chalk.red.bold(country.totalDeaths ? country.totalDeaths : 0)
+  chalk.red.bold(country.totalDeaths ? country.totalDeaths : 0),
 ];
 
-axios
-  .get("https://www.bing.com/covid/data", {
-    headers: {
-      "Accept-Language": "en-US"
-    }
-  })
-  .then(response => response.data)
-  .then(response => {
-    countdown.stop();
+fetchData.then((response) => {
+  countdown.stop();
 
-    const table = new Table({
-      head: [
-        chalk.black.bold("Area"),
-        chalk.black.bold("Total Confirmed"),
-        chalk.black.bold("Total Recovered"),
-        chalk.black.bold("Total Death")
-      ],
-      colWidths: [30, 30, 30, 30]
-    });
-
-    if (argv.country) {
-      var country = response.areas.find(
-        area => area[filterProp].indexOf(argv.country) >= 0
-      );
-    }
-
-    if (country) {
-      table.push([country[displayProp], ...getValue(country)]);
-    } else {
-      table.push(["Worldwide", ...getValue(response)]);
-
-      response.areas.forEach(area => {
-        table.push([area[displayProp], ...getValue(area)]);
-      });
-    }
-
-    console.log(table.toString());
-
-    process.exit(0);
+  const table = new Table({
+    head: [
+      chalk.black.bold("Area"),
+      chalk.black.bold("Total Confirmed"),
+      chalk.black.bold("Total Recovered"),
+      chalk.black.bold("Total Death"),
+    ],
+    colWidths: [30, 30, 30, 30],
   });
+
+  if (argv.country) {
+    var country = response.areas.find(
+      (area) => area[filterProp].indexOf(argv.country) >= 0
+    );
+  }
+
+  if (country) {
+    table.push([country[displayProp], ...getValue(country)]);
+  } else {
+    table.push(["Worldwide", ...getValue(response)]);
+
+    response.areas.forEach((area) => {
+      if (area[displayProp]) {
+        table.push([area[displayProp], ...getValue(area)]);
+      }
+    });
+  }
+
+  console.log(table.toString());
+
+  process.exit(0);
+});
---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  filterProp: "displayName",
+  displayProp: "displayName",
+};
